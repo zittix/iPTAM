44add25897f44262172ebd72ae3712dcaf99a5b9
Mathieu Monney <mathieu@MacBook-Pro-de-Mathieu.local>
2013-12-04 15:22:07 +0100

    Added PTAM

---
+++ PTAM-GPL/Src/SettingsHelper.h
@@ -0,0 +1,16 @@
+//
+//  SettingsHelper.h
+//  xPTAM
+//
+//  Created by Mathieu Monney on 04.12.13.
+//
+//
+
+#ifndef xPTAM_SettingsHelper_h
+#define xPTAM_SettingsHelper_h
+
+inline double getSettingsDouble(const char* name, double defaultSettings) {
+    return defaultSettings;
+}
+
+#endif
